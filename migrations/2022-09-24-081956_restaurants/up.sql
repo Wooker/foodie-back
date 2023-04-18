5004abab6d05eb75fb1bf074aafca876ae4dd05f
wooker <zakhar.semenov@nu.edu.kz>
2023-04-19 02:07:49 +0600

    update image urls

--- migrations/2022-09-24-081956_restaurants/up.sql
+++ migrations/2022-09-24-081956_restaurants/up.sql
@@ -22,10 +22,10 @@ CREATE TABLE "restaurant_location" (
 ALTER TABLE "restaurant_location" ADD FOREIGN KEY ("restaurant_info_id") REFERENCES "restaurant_info" ("id");
 
 INSERT INTO restaurant_info VALUES
-('85d436f9-3500-4a4b-abea-840fd5e044ec', 'Zina', 'Zina restaurant - "В гостях у Бабушки Зины"', 'Akyrtas street, 1/1', '08:00-24:00', 4, 4.32, 'http://1140427-ci11141.tw1.ru:8080/images/zina.jpg', '+7(777)123-45-67'),
-('344ce739-adb5-41da-8bd2-189db10cad39', 'Qazaq Gourmet', 'Qazaq gourmet restaurant', 'Mangilik el street, 29', '12:00-23:00', 4, 4.53, 'http://1140427-ci11141.tw1.ru:8080/images/qazaq_gourmet.jpg', '+7(777)133-45-67'),
-('4d237a1c-0fd0-4f4a-b395-04b4468ecb14', 'Meat point', 'Halal restaurant', 'Uly dala street, 45', '00:00-24:00', 3, 4.19, 'http://1140427-ci11141.tw1.ru:8080/images/meat_point.png', '+7(777)123-55-67'),
-('006f41e5-ab90-4e86-9e74-c381e8220919', 'Lanzhou', 'Chinese restaurant', 'Sarayshik street, 5', '11:00-23:00', 3, 4.4, 'http://1140427-ci11141.tw1.ru:8080/images/lanzhou.png', '+7(777)123-45-77')
+('85d436f9-3500-4a4b-abea-840fd5e044ec', 'Zina', 'Zina restaurant - "В гостях у Бабушки Зины"', 'Akyrtas street, 1/1', '08:00-24:00', 4, 4.32, 'http://1385980-ci11141.tw1.ru:8080/images/zina.jpg', '+7(777)123-45-67'),
+('344ce739-adb5-41da-8bd2-189db10cad39', 'Qazaq Gourmet', 'Qazaq gourmet restaurant', 'Mangilik el street, 29', '12:00-23:00', 4, 4.53, 'http://1385980-ci11141.tw1.ru:8080/images/qazaq_gourmet.jpg', '+7(777)133-45-67'),
+('4d237a1c-0fd0-4f4a-b395-04b4468ecb14', 'Meat point', 'Halal restaurant', 'Uly dala street, 45', '00:00-24:00', 3, 4.19, 'http://1385980-ci11141.tw1.ru:8080/images/meat_point.png', '+7(777)123-55-67'),
+('006f41e5-ab90-4e86-9e74-c381e8220919', 'Lanzhou', 'Chinese restaurant', 'Sarayshik street, 5', '11:00-23:00', 3, 4.4, 'http://1385980-ci11141.tw1.ru:8080/images/lanzhou.png', '+7(777)123-45-77')
 ;
 
 INSERT INTO restaurant_location VALUES
--- migrations/2023-02-26-172401_restaurant_menu/up.sql
+++ migrations/2023-02-26-172401_restaurant_menu/up.sql
@@ -12,6 +12,6 @@ CREATE TABLE "restaurant_menu" (
 ALTER TABLE "restaurant_menu" ADD FOREIGN KEY ("restaurant_info_id") REFERENCES "restaurant_info" ("id");
 
 INSERT INTO "restaurant_menu" VALUES
-(gen_random_uuid(), '006f41e5-ab90-4e86-9e74-c381e8220919', 'Noodles', 'Ramen', 2000, 'Noodles, beef, onion, egg, soup', 'http://1140427-ci11141.tw1.ru:8080/images/Noodles.jpg'),
-(gen_random_uuid(), '4d237a1c-0fd0-4f4a-b395-04b4468ecb14', 'Hamburger', 'FastFood', 2600, 'Beef, salad, red onion, pickles, tomato', 'http://1140427-ci11141.tw1.ru:8080/images/burger.png')
+(gen_random_uuid(), '006f41e5-ab90-4e86-9e74-c381e8220919', 'Noodles', 'Ramen', 2000, 'Noodles, beef, onion, egg, soup', 'http://1385980-ci11141.tw1.ru:8080/images/Noodles.jpg'),
+(gen_random_uuid(), '4d237a1c-0fd0-4f4a-b395-04b4468ecb14', 'Hamburger', 'FastFood', 2600, 'Beef, salad, red onion, pickles, tomato', 'http://1385980-ci11141.tw1.ru:8080/images/burger.png')
 ;
